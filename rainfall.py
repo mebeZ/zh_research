cccf95e46f94c68c72b3541f08f4e6aea5508642
mebeZ <zacharyhine@gmail.com>
2023-04-13 12:53:38 -0400

    New changes

--- rainfall.py
+++ rainfall.py
@@ -2,22 +2,23 @@ import requests
 import pandas as pd
 import csv
 
-query = {'stationId':'KNYNEWYO1348', 'format':'json', 'units':'e','apiKey':'329d28aca92149f19d28aca92139f1ea'}
+query = {'stationId':'KNYNEWYO1348', 'format':'json', 'units':'e', 'apiKey':'329d28aca92149f19d28aca92139f1ea'}
 src = "https://api.weather.com/v2/pws/observations/all/1day"
 res = requests.get(src, params=query).json()
+#print(res)
 
-csvfile = open('rainfall_04_10_2022', 'w', newline='')
+csvfile = open('rainfall_10_01_2018', 'w', newline='')
 rainwriter = csv.writer(csvfile, delimiter=',')
 #write the names of the columns to the csv
 rainwriter.writerow(['time_local', 'time_utc', 'precip_rate', 'precip_total'])
 
 #df = pd.DataFrame(columns=['time_local', 'time_utc', 'precip_rate', 'precip_total'])
-#print(res['observations'][0])
+print(res['observations'][0])
 
 for obs in res['observations']:
     tl = obs['obsTimeLocal']
     tu = obs['obsTimeUtc']
-    pr = obs["imperial"]["precipRate"]
+    pr = obs['imperial']['precipRate']
     pt = obs['imperial']['precipTotal']
     # write a row of data into the csvfile
     rainwriter.writerow([tl, tu, pr, pt])
